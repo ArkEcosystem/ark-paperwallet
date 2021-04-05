ac20ec56130eb1221e752f72057928cc387399c4
faustbrian <faustbrian@users.noreply.github.com>
2021-04-05 03:50:45 +0000

    deploy [ci skip]

---
+++ precache-manifest.7f98b887d4e2315763ad8f05d33d8103.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d31a692126a7d6ae7d0",
+    "url": "/paper-wallet/css/app.d3f2eb0d.css"
+  },
+  {
+    "revision": "4a39d923a40ae82462dd",
+    "url": "/paper-wallet/css/chunk-vendors.dfc4150c.css"
+  },
+  {
+    "revision": "350efcdfb545aea0639e",
+    "url": "/paper-wallet/css/message.ea66ce38.css"
+  },
+  {
+    "revision": "a9f1bdcf1863ea81a877",
+    "url": "/paper-wallet/css/message:sign.fd5b4b94.css"
+  },
+  {
+    "revision": "4fd8cece0924d504b447",
+    "url": "/paper-wallet/css/message:verify.744797de.css"
+  },
+  {
+    "revision": "7c0d3a75a91823b54a3e",
+    "url": "/paper-wallet/css/wallet.187494d1.css"
+  },
+  {
+    "revision": "8e11892017cbdaeb6e4b",
+    "url": "/paper-wallet/css/wallet:entropy.f2dff00a.css"
+  },
+  {
+    "revision": "3be203c7a485eefa6dbe",
+    "url": "/paper-wallet/css/wallet:passphrase.744797de.css"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/paper-wallet/fonts/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/paper-wallet/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/paper-wallet/fonts/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "a8db2e3cdcb207d66a136af0330fc6b2",
+    "url": "/paper-wallet/img/background.a8db2e3c.png"
+  },
+  {
+    "revision": "3d19f3a7040f8445988b3627c2466341",
+    "url": "/paper-wallet/img/globe.3d19f3a7.png"
+  },
+  {
+    "revision": "15f59cb3681a23c5f30073ff221cc6f8",
+    "url": "/paper-wallet/index.html"
+  },
+  {
+    "revision": "6d31a692126a7d6ae7d0",
+    "url": "/paper-wallet/js/app.ef1e069c.js"
+  },
+  {
+    "revision": "4a39d923a40ae82462dd",
+    "url": "/paper-wallet/js/chunk-vendors.7b0ff9db.js"
+  },
+  {
+    "revision": "350efcdfb545aea0639e",
+    "url": "/paper-wallet/js/message.59049c55.js"
+  },
+  {
+    "revision": "a9f1bdcf1863ea81a877",
+    "url": "/paper-wallet/js/message:sign.0109577b.js"
+  },
+  {
+    "revision": "94a776573901496aaf00",
+    "url": "/paper-wallet/js/message:sign~message:verify.f43dc2fd.js"
+  },
+  {
+    "revision": "872074a2a23c79e3591e",
+    "url": "/paper-wallet/js/message:sign~message:verify~wallet:entropy~wallet:passphrase.0d1b9ab1.js"
+  },
+  {
+    "revision": "cbbbd1d748a3ab2a462e",
+    "url": "/paper-wallet/js/message:sign~wallet:entropy~wallet:passphrase.fca7fd72.js"
+  },
+  {
+    "revision": "4fd8cece0924d504b447",
+    "url": "/paper-wallet/js/message:verify.08ab306c.js"
+  },
+  {
+    "revision": "7c0d3a75a91823b54a3e",
+    "url": "/paper-wallet/js/wallet.91efc5ca.js"
+  },
+  {
+    "revision": "8e11892017cbdaeb6e4b",
+    "url": "/paper-wallet/js/wallet:entropy.13cb736b.js"
+  },
+  {
+    "revision": "3be203c7a485eefa6dbe",
+    "url": "/paper-wallet/js/wallet:passphrase.ed58260f.js"
+  },
+  {
+    "revision": "be9a625b7d384d872ea45542dbf9642c",
+    "url": "/paper-wallet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/paper-wallet/robots.txt"
+  }
+]);
